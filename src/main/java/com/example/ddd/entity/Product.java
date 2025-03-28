9deb3b9de4378969093e2aead4c0f173aaa1defc
Gus <g.92oliveira@gmail.com>
2025-03-28 07:06:15 -0300

    create more test

---
+++ src/main/java/com/example/ddd/entity/Product.java
@@ -0,0 +1,35 @@
+package com.example.ddd.entity;
+
+public class Product {
+    private String id;
+    private String name;
+    private int price;
+
+    public Product(String id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.validate();
+    }
+
+    private void validate() {
+        if (this.id.isEmpty()) {
+            throw new IllegalArgumentException("ID is required");
+        }
+        if (this.name.isEmpty()) {
+            throw new IllegalArgumentException("NAME is required");
+        }
+        if (this.price <= 0) {
+            throw new IllegalArgumentException("PRICE must be higher than 0");
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+	public void changeName(String aName) {
+        this.name = aName;
+	}
+
+}
