7bce81ac3d98189db8d1f0e31104bba64b32b52a
Gus <g.92oliveira@gmail.com>
2025-03-28 02:56:05 -0300

    fix tests

--- src/test/java/com/example/ddd/entity/CustomerTest.java
+++ src/test/java/com/example/ddd/entity/CustomerTest.java
@@ -1,12 +1,20 @@
 package com.example.ddd.entity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
-public class CustomerTest {
-    
+class CustomerTest {
+
     @Test
     void givenANewCustomerDoesNotHaveAnIdThenThrowError() {
-        assertThrows(IllegalArgumentException.class, () -> new Customer("", "Gustavo"));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Customer("", "Gustavo"));
+        assertEquals("ID is required", exception.getMessage());
+    }
+
+    @Test
+    void givenACustomerWhenDoesNotHaveNameThenThrowNameIsRequired() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Customer("123", ""));
+        assertEquals("Name is required", exception.getMessage());
     }
 }
